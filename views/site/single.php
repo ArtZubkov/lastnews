9e827f0ed5ce20bd184056f6520b04783ac5b3fa
Art Zubkov <baqfert@gmail.com>
2018-03-01 01:55:02 +0300

    Wrapper

---
+++ views/site/single.php
@@ -0,0 +1,27 @@
+<?php  
+$this->title = 'Lorem Ipsum';
+?>
+
+
+<div class="content">
+	<section class="news">
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-lg-6">
+					<div class="article__full">
+						<img src="<?=''?>" alt="Image" class="article__full__img">
+						<div class="article__full__block">
+							<h3 class="article__full__title"><?='Lorem Ipsum'?></h3>
+							<p class="article__full__text"><?="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"?></p>
+							<time class="article__full__pubdate">
+								<?='pubdate'?>
+							</time>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+</div>
